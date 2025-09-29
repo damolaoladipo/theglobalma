2ec2b32e8d63567f4e99cf3620b485f3c7f31db7
damola <thedamolaoladipo@gmail.com>
2025-09-29 13:39:49 +0100

    chore: development build

---
+++ _data/site-config.tsx
@@ -0,0 +1,28 @@
+export const siteConfig = {
+  name: "theglobalma - learn and grow by doing",
+  title: "theglobalma",
+  url: "https://www.theglobalma.com",
+  getStartedUrl: "https://www.theglobalma.com/login",
+  ogImage: "/blocks/og-image.png",
+  products: {
+    name: "lens",
+    url: "https://lens.theglobalma.com",
+    docs: "https://github.com/theglobalma"
+  },
+  description:
+    "theglobalma helps African tech talents learn and grow by doing. We support the development of Open Source Software that solves problems faced daily by Africans, and we are creating points of entry into machine learning research.",
+  links: {
+    creator: "@theglobalma",
+    twitter: "https://x.com/theglobalma",
+    instagram: "https://instagram.com/theglobalma",
+    linkedin: "https://linkedin.com/company/theglobalma",
+    github: "https://github.com/theglobalma",
+    email: "mailto:hello@theglobalma.com",
+  },
+  contact: {
+    telephone: "+2348170848057",
+    email: "hello@theglobalma",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
